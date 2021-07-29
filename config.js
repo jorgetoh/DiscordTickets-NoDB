034cd9768b7de6e89f15eb87fb02ecb7a813ad0b
jorgetoh <77821204+jorgetoh@users.noreply.github.com>
2021-07-29 10:59:20 +0200

    Update config.js

--- config.js
+++ config.js
@@ -3,14 +3,45 @@ const config = require('./config.json')
 
 var ticketAmount = 0
 
-function createNewTicket(module, guild) {
+function createNewTicket(module, guild, user) {
     ticketAmount += 1
     guild.channels.create(`${module['prefix']}-${ticketAmount}`, {
         type: 'text',
         parent: module['tickets-categoryID'],
-        permissionOverwrites: []
-    })
+        permissionOverwrites: [
+            {
+                id: guild.roles.everyone.id,
+                deny: ['VIEW_CHANNEL'],
+            },
+            {
+                id: user.id,
+                allow: ['VIEW_CHANNEL'],
+            },
+        ]
+    }).then(channel => {
 
+        let strRoles = 'The role(s): '
+        let roleList = module['rolesID']
+        for (j in roleList) {
+            strRoles = strRoles.concat(`<@&${roleList[j]['roleID']}>, `)
+            channel.updateOverwrite(guild.roles.cache.get(roleList[j]['roleID']), { VIEW_CHANNEL: true })
+
+        }
+        strRoles = strRoles.substring(0, strRoles.length-2)
+        strRoles = strRoles.concat(' can view your ticket.')
+
+        let embedMessage = new Discord.MessageEmbed()
+            .setTitle(`Ticket #${ticketAmount} (${module.prefix})`)
+            .setDescription(`${user} thanks you for contacting us. Help will be delivered soon.`)
+            .setThumbnail(module['creationEmbed']['embedThumbnail'])
+            .setColor(module['creationEmbed']['embedColor'])
+        
+        embedMessage.addField('STAFF Contacted', strRoles)
+
+        channel.send(embedMessage).then(message => {
+            message.react('🔒')
+        })
+    })
 }
 
 function setClientInfo(client) {
@@ -25,15 +56,14 @@ function setClientInfo(client) {
     client.user.setUsername(config.bot.info.clientInfo.name)
 }
 
-function getEmbedMessage(module) {
-    var embedMessage = new Discord.MessageEmbed()
-
-    embedMessage.setTitle(module['embedMsg']['embedTitle'])
-    embedMessage.setDescription(module['embedMsg']['embedDesc'])
-    embedMessage.setThumbnail(module['embedMsg']['embedThumbnail'])
-    embedMessage.setColor(module['embedMsg']['embedColor'])
+function getCreationEmbed(module) {
+    let embedMessage = new Discord.MessageEmbed()
+        .setTitle(module['creationEmbed']['embedTitle'])
+        .setDescription(module['creationEmbed']['embedDesc'])
+        .setThumbnail(module['creationEmbed']['embedThumbnail'])
+        .setColor(module['creationEmbed']['embedColor'])
     
-    var embedFields = module['embedMsg']['embedFields']
+    let embedFields = module['creationEmbed']['embedFields']
     for (j in embedFields) {
         embedMessage.addField(embedFields[j]['name'], embedFields[j]['value'])
     }
@@ -46,7 +76,7 @@ function getBotModules() {
 
 module.exports = {
     setClientInfo,
-    getEmbedMessage,
+    getCreationEmbed,
     getBotModules,
     createNewTicket
 }
