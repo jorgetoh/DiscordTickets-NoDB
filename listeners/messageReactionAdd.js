a83c487a2502ec36579178c685a012ce47926bd8
jorgetoh <77821204+jorgetoh@users.noreply.github.com>
2021-07-29 10:58:26 +0200

    Update messageReactionAdd.js

--- listeners/messageReactionAdd.js
+++ listeners/messageReactionAdd.js
@@ -10,19 +10,36 @@ module.exports = async (bot, reaction, user) => {
     }
   
     let message = reaction.message
+
     if (!message) {
         return
     } 
     if (user.bot) {
         return
     }
-
-    reaction.users.remove(user.id)
-    var botModules = config.getBotModules()
-    for (i in botModules) {
-        if (botModules[i]['creation-channelID'] === message.channel.id) {
-            config.createNewTicket(botModules[i], message.guild)
-            return;
-        }
+    switch (reaction.emoji.name) {
+        case '📩':
+            var botModules = config.getBotModules()
+            for (i in botModules) {
+                if (botModules[i]['creation-channelID'] === message.channel.id) {
+                    reaction.users.remove(user.id)
+                    config.createNewTicket(botModules[i], message.guild, user)
+                    return
+                }
+            }
+            break
+        case '🔒':
+            var botModules = config.getBotModules()
+            for (i in botModules) {
+                if (botModules[i]['tickets-categoryID'] === message.channel.parent.id) {
+                    reaction.users.remove(user.id)
+                    message.channel.send('Removing ticket in 5 seconds...')
+                    setTimeout(function(){ 
+                        message.channel.delete()
+                     }, 5000);
+                    return
+                }
+            }
+            break
     }
 }
